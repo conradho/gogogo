8ea22b54549ffabbf15034c70528d6eebd025232
conrad <conrad@pythonanywhere.com>
2017-11-20 12:52:48 +0000

    refactor out function to handle single inbound connection

--- pkg/mynet/portforward.go
+++ pkg/mynet/portforward.go
@@ -10,26 +10,35 @@ import (
 func Forward(port int, target string) {
 	log.Printf("About to forward traffic from port %d to %s", port, target)
 
-	incoming, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("could not start server on %d: %v", port, err)
 	}
 
 	for {
-		client, err := incoming.Accept()
-		if err != nil {
-			log.Fatal("could not accept client connection", err)
-		}
-		defer client.Close()
-		log.Printf("client '%v' connected!\n", client.RemoteAddr())
-
-		forwardConnection, err := net.Dial("tcp", target)
-		if err != nil {
-			log.Fatal("could not connect to target", err)
-		}
-		defer forwardConnection.Close()
-		log.Printf("connection to server %v established!\n", forwardConnection.RemoteAddr())
-		go func() { io.Copy(forwardConnection, client) }()
-		go func() { io.Copy(client, forwardConnection) }()
+		ForwardIncomingConnection(l, target)
 	}
+}
+
+func ForwardIncomingConnection(l net.Listener, target string) {
+	inboundConn, err := l.Accept()
+	if err != nil {
+		log.Fatal("could not accept client connection", err)
+	}
+	log.Printf("client '%v' connected!\n", inboundConn.RemoteAddr())
+
+	outboundConn, err := net.Dial("tcp", target)
+	if err != nil {
+		log.Fatal("could not connect to target", err)
+	}
+	log.Printf("outbound connection to server %v established!\n", outboundConn.RemoteAddr())
+
+	go func() {
+		io.Copy(inboundConn, outboundConn)
+		inboundConn.Close()
+	}()
+	go func() {
+		io.Copy(outboundConn, inboundConn)
+		outboundConn.Close()
+	}()
 }
